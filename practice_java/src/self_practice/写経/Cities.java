d7c9d98e83af164dd0aee8d58aa6121934a83b6d
eeb-tosh-16 <eeb-tosh-16@DESKTOP-NT4PNTH>
2020-07-13 16:07:03 +0900

    7/13eeb

---
+++ practice_java/src/self_practice/写経/Cities.java
@@ -0,0 +1,29 @@
+package self_practice.写経;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Cities {
+
+	public static void findChicagoImperative(final List<String> cities) {
+		boolean found = false;
+		for (String city : cities) {
+			if(city.equals("Chicago")) {
+				found = true;
+				break;
+			}
+		}
+		System.out.println("Found chicago?:" + found);
+	}
+
+	public static void findChicagoDeclarative(final List<String> cities) {
+		System.out.println("Found chicago?:" + cities.contains("Chicago"));
+	}
+
+	public static void main(final String[] args) {
+		List<String> cities = Arrays.asList("Albany", "Boulder", "Denver", "Eugene");
+
+		findChicagoImperative(cities);
+		findChicagoDeclarative(cities);
+	}
+}
